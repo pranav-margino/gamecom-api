0e6e1805721fc9b6bbf000d8e8ee591c455fe1e7
pranav kumar <pranav.fullstack@gmail.com>
2016-05-29 22:57:44 +0530

    next commit

--- common/models/vendoruser.js
+++ common/models/vendoruser.js
@@ -1,5 +1,5 @@
-var bcrypt = require('bcrypt');
 var nodemailer = require('nodemailer');
+var app = require('../../server/server');
 module.exports = function(VendorUser) {
     VendorUser.existsEmail = function(email, cb) {
         VendorUser.find({
@@ -19,9 +19,6 @@ module.exports = function(VendorUser) {
             return cb(err, result)
         });
     };
-    /*VendorUser.logIn = function(email,password,cb){
-        cb(false,true);
-    }*/
     VendorUser.remoteMethod('existsEmail', {
         http: {
             path: '/existsEmail',
@@ -50,61 +47,18 @@ module.exports = function(VendorUser) {
             type: 'object'
         }
     });
-    /*VendorUser.remoteMethod('logIn', {
-        http: {
-            path: '/logIn',
-            verb: 'post'
-        },
-        accepts: [{
-            arg: 'email',
-            type: 'string'
-        },{
-            arg:'password',
-            type:'string'
-        }],
-        returns: {
-            arg: 'users',
-            type: 'object'
-        }
-    });
-*/
-    /*VendorUser.observe('before save', function(ctx, next) {
-        if (!ctx.isNewInstance) {
-            next();
-        };
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(ctx.instance.password, salt, function(err, hash) {
-                ctx.instance.password = hash;
+    VendorUser.observe('before save', function(ctx, next) {
+        if (ctx.isNewInstance) {
+            var Vendor = app.models.Vendor;
+            var slug = parseInt(Math.random() * 100000);
+            Vendor.create({
+                name: 'Untitled Vendor ' + slug
+            }, function(err, data) {
+                ctx.instance.vendorId = data.id;
                 next();
             });
-        });
-    });
-    */
-    VendorUser.observe('after save', function(ctx, next) {
-        if (ctx.isNewInstance) {
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'pranav.virgo.1989@gmail.com',
-                    pass: 'newgmailpasswordP2'
-                },
-                debug: true
-            });
-            var mailOptions = {
-                from: '"PK" <pranav.virgo.1989@gmail.com>',
-                to: ctx.instance.email,
-                subject: 'Welcome to Heaven',
-                text: 'mWWWAAAHHHH XOXOX',
-                html: '<b>Hello world</b>'
-            };
-            /*transporter.sendMail(mailOptions, function(error, info) {
-                if (error) {
-                    return console.log(error);
-                }
-                console.log('Message sent: ' + info.response);
-            });
-            */
+        } else {
+            next();
         }
-        next();
     });
 };
