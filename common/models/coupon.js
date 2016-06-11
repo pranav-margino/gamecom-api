c5bb9e3138b817465c2759bac6e2beb3484e6130
pranav kumar <pranav.fullstack@gmail.com>
2016-06-11 11:53:54 +0530

    next commit

--- common/models/coupon.js
+++ common/models/coupon.js
@@ -1,3 +1,20 @@
-module.exports = function(Coupon){
-	
+var app = require('../../server/server');
+module.exports = function(Coupon) {
+    Coupon.observe('after save', function(ctx, next) {
+        var instance = ctx.instance;
+        var Vendor = app.models.Vendor;
+        var Store = app.models.Store;
+        Vendor.findById(instance.vendorId, {
+            include: "stores"
+        }, function(err, vendor) {
+            var idArray = [];
+            vendor.stores(function(err, stores) {
+                for (var i = 0; i < stores.length; i++) {
+                    idArray.push(stores[i].id);
+                };
+                Store.addCoupon(idArray, instance);
+            });
+        });
+        next();
+    });
 }
--- common/models/store.js
+++ common/models/store.js
@@ -1,3 +1,16 @@
-module.exports = function(Store){
-	
+var _ = require('lodash');
+module.exports = function(Store) {
+    Store.addCoupon = function(idArray, coupon) {
+        _.forEach(idArray, function(id) {
+            Store.findById(id, function(err, store) {
+                store.storeCoupons.exists({
+                    "where": {
+                        "modelName": "Coupon"
+                    }
+                }, function(err, coupon) {
+                    console.log(coupon);
+                });
+            });
+        });
+    }
 }
