ae8027cb82aea46946d8b5a32a7011719566ccbd
pranav kumar <pranav.fullstack@gmail.com>
2016-06-19 15:23:57 +0530

    next commit

--- common/models/vendor.js
+++ common/models/vendor.js
@@ -1,3 +1,35 @@
+var _ = require('lodash');
+var async = require('async');
 module.exports = function(Vendor) {
-
+    Vendor.getNames = function(idsArray, cb) {
+    	console.log(idsArray);
+        async.map(idsArray, function(id, callback) {
+            Vendor.findById(id, {
+                fields: {
+                    id: true,
+                    name: true,
+                    displayName: true,
+                    legalName: true
+                }
+            }, function(err, vendor) {
+                callback(null, vendor);
+            })
+        }, function(err, result) {
+            return cb(err, result);
+        });
+    }
+    Vendor.remoteMethod('getNames', {
+        http: {
+            path: '/getNames',
+            verb: 'post'
+        },
+        accepts: {
+            arg: 'idsArray',
+            type: 'array'
+        },
+        returns: {
+            arg: 'vendors',
+            type: 'array'
+        }
+    });
 };
