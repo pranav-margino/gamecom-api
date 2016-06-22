924f6617a2e234999abb75664295908c912d55e5
pranav kumar <pranav.fullstack@gmail.com>
2016-06-22 11:02:55 +0530

    next commit

--- common/models/container.js
+++ common/models/container.js
@@ -3,12 +3,14 @@ module.exports = function(Container) {
     Container.afterRemote('upload', function(ctx, res, next) {
         var result = res.result;
         var file = result.files.file[0];
+        console.log(result);
         var fields = result.fields;
         var model = getModel(fields.model.toString());
         if (model) {
             model.findById(fields.id.toString(), function(err, instance) {
                 if (!err) {
-                    instance.brandLogo.create({
+                    var fileModel = getFileModel(fields.type.toString(), instance);
+                    fileModel.create({
                         format: file.type,
                         size: file.size,
                         name: file.name
@@ -28,9 +30,27 @@ module.exports = function(Container) {
             case 'Store':
                 model = app.models.Store;
                 break;
+            case 'Coupon':
+            	model = app.models.Coupon;
+            	break;    
             default:
                 break;
         }
         return model;
+    }
+
+    function getFileModel(fileModelName, modelInstance) {
+        var fileModel;
+        switch (fileModelName) {
+            case 'brandLogo':
+                fileModel = modelInstance.brandLogo;
+                break;
+            case 'featuredImage':
+                fileModel = modelInstance.featuredImage;
+                break;
+            default:
+                break;
+        }
+        return fileModel;
     }
 };
