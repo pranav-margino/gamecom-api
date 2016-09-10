f092495154a21f72b662ddb27e5355c596e93602
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2016-09-10 16:37:54 +0530

    next commit

--- common/models/answer.js
+++ common/models/answer.js
@@ -1,22 +1,27 @@
 var io = require('../../modules/io');
+var logger = require('../../modules/logger');
+var queue = require('../../modules/queue');
+
 
 module.exports = function(Answer) {
     io.on('ready', function(socket) {
         socket.on('addModel:Answer', function(data) {
-            console.log(data);
+            logger.log('addModel:Answer');
+            logger.log('debug', data);
+            queue.push('game0', data);
         });
     });
 
 
     Answer.observe('after save', function(ctx, next) {
         if (ctx.isNewInstance) {
-            console.log(ctx.instance);
+            logger.log(ctx.instance);
             producer.send([{
                 topic: 'game0',
                 messages: [JSON.stringify(ctx.instance)]
             }], function(err, data) {
-                console.log(err);
-                console.log(data);
+                logger.log(err);
+                logger.log(data);
             });
         }
         next();
