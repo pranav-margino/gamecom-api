369b8fa03371d83afb359800b26939de2514df1e
pranav kumar <pranavkumar@pranavs-MacBook-Air.local>
2016-12-20 17:55:49 +0530

    next commit

--- common/models/vendor.js
+++ common/models/vendor.js
@@ -1,5 +1,10 @@
 var _ = require('lodash');
 var async = require('async');
+var app = require('../../server/server');
+var debug = require('debug')('favourite');
+var colors = require('colors');
+
+
 module.exports = function(Vendor) {
     Vendor.getNames = function(ids, cb) {
         ids = Object.prototype.toString.call(ids) == '[object Array]' ? ids : [ids];
@@ -19,6 +24,88 @@ module.exports = function(Vendor) {
             return cb(err, result);
         });
     }
+
+
+    Vendor.getConsumers = function(id, cb) {
+        app.models.Favourite.find({ where: { 'product.vendorId': id }, order: 'createdAt DESC' }, function(err, favourites) {
+            var consumers = _.map(favourites, function(favourite) {
+                return _.merge(favourite.user, { product: favourite.product, lastSeen: favourite.createdAt });
+            });
+
+
+            return cb(err, _.uniqBy(consumers, function(consumer) {
+                return consumer.id;
+            }));
+        })
+    }
+
+    Vendor.getProducts = function(id, cb) {
+        app.models.Favourite.find({ where: { 'product.vendorId': id }, order: 'createdAt DESC' }, function(err, favourites) {
+            var products = _.map(favourites, function(favourite) {
+                return favourite.product;
+            });
+
+
+            return cb(err, _.uniqBy(products, function(product) {
+                return product.id;
+            }));
+
+        })
+    }
+
+    Vendor.remoteMethod('getProducts', {
+        http: {
+            path: '/getProducts',
+            verb: 'GET'
+        },
+        accepts: {
+            arg: 'id',
+            type: 'string'
+        },
+        returns: {
+            arg: 'products',
+            type: 'array'
+        }
+    });
+
+    Vendor.remoteMethod('getConsumers', {
+        http: {
+            path: '/getConsumers',
+            verb: 'GET'
+        },
+        accepts: {
+            arg: 'id',
+            type: 'string'
+        },
+        returns: {
+            arg: 'consumers',
+            type: 'array'
+        }
+    });
+
+    Vendor.getProductsConsumers = function(id, cb) {
+        app.models.Favourite.find({ where: { 'product.vendorId': id } }, function(err, favourites) {
+            return cb(err, _.groupBy(favourites, function(favourite) {
+                return favourite.product.id;
+            }));
+        })
+    }
+
+    Vendor.remoteMethod('getProductsConsumers', {
+        http: {
+            path: '/getProductsConsumers',
+            verb: 'GET'
+        },
+        accepts: {
+            arg: 'id',
+            type: 'string'
+        },
+        returns: {
+            arg: 'consumers',
+            type: 'array'
+        }
+    });
+
     Vendor.remoteMethod('getNames', {
         http: {
             path: '/getNames',
